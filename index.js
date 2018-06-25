a6ccf109bb0b1cc75b0ba3f4fa2dc1655676f9c9
deyvessh <deyvesshkumar@gmail.com>
2018-06-25 17:35:28 +0530

    equality

--- index.js
+++ index.js
@@ -4,9 +4,11 @@
 */
 
 
-// variables
+// Equality operator 
+// "==" - when both variables have same value, whether of not same data type. will return boolean value - "true" 
+// "===" - both variable should hava same value and same data type. 
 
-var x = 5;
-var y = 10;
-
-var total = x + y;
+var foo = 67;
+var loo = 67;
+var baz = "67";
+var laz = "sixty seven"
