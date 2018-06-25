a434b3705d78efdda538b1ef43bbe3b59936f2b7
deyvessh <deyvesshkumar@gmail.com>
2018-06-25 18:22:01 +0530

    advance operator

--- index.js
+++ index.js
@@ -4,16 +4,25 @@
 */
 
 
-// operator
+// advance operator
 
-var a = 45.5656;
-var b = 44.45445;
-var c = 56556;
-var d = -323.4545;
+// modulus
 
-var add = a + b;
-var sub = b - c;
-var mul = d * b;
-var div = a / b;
+var x = 5;
 
-var x = ((a + b) / c * d);
+var y = x % 2;
+
+// increment
+
+var a = 56;
+
+var b = a++;
+// var c = ++a;
+
+
+// decrement
+
+var p = 5;
+
+// var q = p--;
+var r = --p;
