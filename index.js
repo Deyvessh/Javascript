a3fbebc29b0badc6fa8d00de7610d14ae6cda02b
deyvessh <deyvesshkumar@gmail.com>
2018-06-25 22:01:59 +0530

    condition 'else'

--- index.js
+++ index.js
@@ -4,56 +4,15 @@
 */
 
 
-// conditions
+// condition
+// else
 
-var country = 'India';
-var weather;
-var food;
-var currency;
 
-if (country == 'America'){
-    weather = 'Warm';
-    food = 'Not that great!';
-    currency = 'USD';
+var umbrellaMandatory;
+
+if (country == 'England'){
+    umbrellaMandatory = true;
+} else{
+    umbrellaMandatory = false;
 }
 
-if (country == 'India'){
-    weather = 'Cool';
-    food = 'Spicy';
-    currency = 'IND';
-}
-
-if (country == 'Dubai'){
-    weather = 'Humid';
-    food = 'Worst taste';
-    currency = 'Dinar';
-}
-
-var final = 'I was in ' + country + ', The weather was ' + weather + ' and food was ' + food + ' , the currency of india is ' + currency + '.';
-
-//assignment
-
-var Name = 'Devesh';
-var Employee_code;
-var Salary;
-var Department;
-
-if (Name == 'Ranjan'){
-    Employee_code = 454;
-    Salary = 749857;
-    Department = 'Sales'; 
-}
-
-if (Name == 'Devesh'){
-    Employee_code = 434;
-    Salary = 5000000;
-    Department = 'Devoloper'; 
-}
-
-if (Name == 'Manish'){
-    Employee_code = 54;
-    Salary = 450970;
-    Department = 'HR'; 
-}
-
-var code = 'Employee name is ' + Name + ', his employee code is ' + Employee_code + ' and his salary per annum is ' + Salary + ', he works at the ' + Department + ' department.';
