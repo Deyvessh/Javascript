333f99e6aaf27153e3c3946499cc05f9078f84b4
deyvessh <deyvesshkumar@gmail.com>
2018-06-25 20:35:43 +0530

    assignment conditional 'if'

--- index.js
+++ index.js
@@ -30,3 +30,30 @@ if (country == 'Dubai'){
 }
 
 var final = 'I was in ' + country + ', The weather was ' + weather + ' and food was ' + food + ' , the currency of india is ' + currency + '.';
+
+//assignment
+
+var Name = 'Devesh';
+var Employee_code;
+var Salary;
+var Department;
+
+if (Name == 'Ranjan'){
+    Employee_code = 454;
+    Salary = 749857;
+    Department = 'Sales'; 
+}
+
+if (Name == 'Devesh'){
+    Employee_code = 434;
+    Salary = 5000000;
+    Department = 'Devoloper'; 
+}
+
+if (Name == 'Manish'){
+    Employee_code = 54;
+    Salary = 450970;
+    Department = 'HR'; 
+}
+
+var code = 'Employee name is ' + Name + ', his employee code is ' + Employee_code + ' and his salary per annum is ' + Salary + ', he works at the ' + Department + ' department.';
