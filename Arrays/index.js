593b6d6a7d429739a00cef66fb256f45e8496197
deyvessh <deyvesshkumar@gmail.com>
2018-06-26 16:57:56 +0530

    arrays

---
+++ Arrays/index.js
@@ -0,0 +1,20 @@
+// var array = [1, 2, 3, 4, 5];
+// console.log(array)
+
+var car = ["BMW", "Audi", "Suzuki", "Hundai", "Lamborgini"];  // array   
+
+
+// car.length = 5
+// indexing starts from 0
+
+
+// indices --- indexes (positions) ----
+
+var BMW = car[0];
+var Audi = car[1];
+var Suzuki = car[2];
+var Hundai = car[3];
+var Lamborgini = car[4];
+
+
+
